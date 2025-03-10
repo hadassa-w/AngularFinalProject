131e29407ccbe84f770fb17e932d531b1025da24
hadassa-w <wh.33681@gmail.com>
2025-03-10 17:01:11 +0200

    update home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,12 @@ import { CoursesListComponent } from '../components/courses-list/courses-list.co
 import { CourseManagementComponent } from '../components/course-management/course-management.component';
 import { LessonsListComponent } from '../components/lessons-list/lessons-list.component';
 import { AddLessonComponent } from '../components/add-lesson/add-lesson.component';
-
+import { RouterModule } from '@angular/router';
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // הפניה לנתיב login כאשר הנתיב ריק
-  { path: 'register', component: RegisterComponent },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' }, // הפניה לנתיב login כאשר הנתיב ריק
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'coursesList', component: CoursesListComponent },
   { path: 'lessonsList', component: LessonsListComponent },
   { path: 'manageCourses', component: CourseManagementComponent },
